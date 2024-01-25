288a00cf1916eda0d1ad3baecaee50aebf90d92c
mdhossain07 <mrabir8097@gmail.com>
2024-01-25 19:42:44 +0600

    sidebar & movie list component added

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
 import "./App.css";
+import MovieList from "./Components/Cine/MovieList";
 import Footer from "./Components/Footer";
 import Navbar from "./Components/Navbar";
+import Sidebar from "./Components/Sidebar";
 
 function App() {
   return (
     <>
       <Navbar />
+      <div className="container grid lg:grid-cols-[218px_1fr] gap-[3.5rem]">
+        <Sidebar />
+        <MovieList />
+      </div>
+
       <Footer />
     </>
   );
