aeb3c521368e7721ca508fcd66bed1db40587b0e
mdhossain07 <mrabir8097@gmail.com>
2024-01-28 15:34:11 +0600

    theme switcher implemented

--- src/Components/Page.jsx
+++ src/Components/Page.jsx
@@ -1,11 +1,14 @@
+import { useContext } from "react";
 import MovieList from "./Cine/MovieList";
 import Footer from "./Footer";
 import Navbar from "./Navbar";
 import Sidebar from "./Sidebar";
+import { ThemeContext } from "../context";
 
 const Page = () => {
+  const { darkTheme } = useContext(ThemeContext);
   return (
-    <div>
+    <div className={`w-full h-full ${darkTheme ? "dark" : ""}`}>
       <Navbar />
       <div className="container grid lg:grid-cols-[218px_1fr] gap-[3.5rem]">
         <Sidebar />
